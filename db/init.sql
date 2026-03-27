bbcb02ddd9b10a3a0715243adc901d540cd14445
hanae-kiki <kikihanae5@gmail.com>
2026-03-27 10:19:15 +0100

    a

---
+++ db/init.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE IF NOT EXISTS flaskdb;
+USE flaskdb;
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100)
+);
+
+INSERT INTO users (name) VALUES ('Salim'), ('Nour'), ('Youssef');
