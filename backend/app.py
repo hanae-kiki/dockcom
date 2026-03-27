c642ba5fc6a057ab5232a037555f7398623f6f01
hanae-kiki <kikihanae5@gmail.com>
2026-03-27 10:27:20 +0100

    a

--- backend/app.py
+++ backend/app.py
@@ -11,14 +11,12 @@ DB_USER = os.getenv("DB_USER", "root")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "root")
 DB_NAME = os.getenv("DB_NAME", "flaskdb")
 
+
 @app.route("/api/users")
 def get_users():
     try:
         conn = mysql.connector.connect(
-            host=DB_HOST,
-            user=DB_USER,
-            password=DB_PASSWORD,
-            database=DB_NAME
+            host=DB_HOST, user=DB_USER, password=DB_PASSWORD, database=DB_NAME
         )
 
         cursor = conn.cursor(dictionary=True)
@@ -31,6 +29,7 @@ def get_users():
 
     except Exception as e:
         return jsonify({"error": str(e)})
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
